5c848bac5680c6c3cf11d6b4f8a940fc7696b1b4
KrolKlaudia <74015760+KrolKlaudia@users.noreply.github.com>
2021-01-20 18:11:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ def main():
         point = line.strip().split()
         points.append(Point(point[0], point[1], point[2]))
 
-    length = 0
-    azimuth = 0
+    length = points[0].get_length(points[1])
+    azimuth = points[0].get_azimuth(points[1])
+    angle = 0
     for i in range(len(points)-1):
         azimuth += points[i].get_azimuth(points[i+1])
         length += points[i].get_length(points[i+1])
+        angle += points[i].get_angle(points[i-1],points[i+1])
 
     print(f'length = {length}')
     print(f'azimuth = {azimuth}')
+    print(f'angle = {angle}')
 
 
 if __name__ == '__main__':
